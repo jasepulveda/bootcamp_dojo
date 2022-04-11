604d90f860de530309d44c01eb3223b50c17d27f
Jaime Sepulveda Aguilera <jaimesepulvedaaguilera@LT00980.local>
2022-04-11 19:49:33 -0400

    Ejercicio FizzBuzz

---
+++ FizzBuzz.py
@@ -0,0 +1,15 @@
+# Título: Cálculo Hipoteca
+# Autor: Jaime Sepúlveda
+# [Bootcamp Data Sciente - Coding Dojo]
+
+TITULO = " FizzBuzz "
+
+for i in range(1,31):
+    if i % 3 == 0 and i % 5 == 0:
+        print("FizzBuzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    elif i % 5 == 0:
+        print("Buzz")
+    else:
+        print(i)
