9c2b40b8bdbf370da5db53f3081a4b8509481448
Jaime Sepulveda Aguilera <jaimesepulvedaaguilera@LT00980.local>
2022-04-11 19:19:13 -0400

    Ejercicio Calculo Pago Hipoteca

---
+++ CalculoHipoteca.py
@@ -0,0 +1,22 @@
+# Título: Cálculo Hipoteca
+# Autor: Jaime Sepúlveda
+# [Bootcamp Data Sciente - Coding Dojo]
+
+TITULO = " Cálculo de Pago Hipoteca "
+p = 400000
+r = 3 / 100
+n = 360
+
+v1 = (r * (1 + r) ** n)
+v2 = (1 + r) ** n - 1
+v3 = (v1 / v2)
+pmensual = p * v3
+
+print("\n\n")
+print(TITULO.center(50, "="))
+print(f"Capital       US$    :  {p:,.2f}")
+print(f"Tasa                 :  {r}")
+print(f"Nro de Cuota Mensual :  {n}")
+print(f"Pago Mensual  US$    :  {pmensual:,.2f}")
+print ("=" * 50)
+print("\n\n")
